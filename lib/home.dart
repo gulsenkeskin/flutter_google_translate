5937928e60b867b7473f2753978f6763159a7879
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2022-09-30 10:44:34 +0300

    added translation

--- lib/home.dart
+++ lib/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:translator/translator.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -8,10 +9,46 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  String translated = 'Translation';
   @override
-  Widget build(BuildContext context) {
-    return Container(
-
-    );
-  }
+  Widget build(BuildContext context) => Scaffold(
+        appBar: AppBar(
+          leading: const Icon(Icons.translate),
+          title: const Text('Translation'),
+        ),
+        body: Card(
+          margin: const EdgeInsets.all(12),
+          child: ListView(
+            padding: const EdgeInsets.all(20),
+            children: [
+              const Text('English (US)'),
+              const SizedBox(
+                height: 8,
+              ),
+              TextField(
+                style:
+                    const TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
+                decoration: const InputDecoration(hintText: 'Enter text'),
+                onChanged: (text) async {
+                  
+                final translation=  await text.translate(from: 'en', to: 'es');
+                setState(() {
+                  translated=translation.text;
+                });
+                },
+              ),
+              const Divider(
+                height: 32,
+              ),
+              Text(
+                translated,
+                style: const TextStyle(
+                    fontSize: 36,
+                    color: Colors.lightBlueAccent,
+                    fontWeight: FontWeight.bold),
+              )
+            ],
+          ),
+        ),
+      );
 }
