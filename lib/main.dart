5e2caf46a5bdefe4c1e26dc30f5fdfe14add8186
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2022-09-30 10:44:19 +0300

    added dark mode

--- lib/main.dart
+++ lib/main.dart
@@ -12,11 +12,17 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      themeMode: ThemeMode.dark,
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primaryColor: Colors.black,
+        scaffoldBackgroundColor: Colors.blueGrey.shade900,
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+        ),
       ),
-      home: const HomePage()
+      home:const HomePage(),
     );
   }
 }
